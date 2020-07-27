b603b7cda58ba97ebd23538544823d9069750b09
Shiwei He <irene@Shiweis-MacBook-Air.local>
2020-07-27 19:58:04 -0400

    installing Axios to send HTTP req to backend

---
+++ src/components/todos-list.component.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { link } from 'react-router-dom';
+import axios from 'axios';
+
+export default class TodosList extends Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome to Todos List Component!!</p>
+      </div>
+    )
+  }
+}
